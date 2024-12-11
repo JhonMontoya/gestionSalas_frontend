4fb127efcdd5ab978581f22dc57f845fe09e3a5b
Jhonathan <jhonathan.guerrero.96@gmail.com>
2024-12-11 02:17:51 -0500

    Merge Jhonathan Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,29 +9,6 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import logo from "../assets/Logo.png";
 
 export default function NavBar() {
-    return (
-        <AppBar position="static" className="navbar">
-            <Toolbar className="navbar-toolbar">
-                <Box className="navbar-logo">
-                    <img
-                        src={logo}
-                        alt="Logo"
-                        className="logo-image"
-                    />
-                </Box>
-                <SearchBar />
-                <Box className="navbar-buttons">
-                    <Button color="inherit" className="navbar-button">
-                        Iniciar Sesion
-                    </Button>
-                    <Button color="inherit" variant="outlined" className="navbar-button">
-                        Registro
-                    </Button>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
-export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{backgroundColor: "#8787f5"}}>
