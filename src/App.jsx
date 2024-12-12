3c03f3eda127c9cd3a185d2f90d7214e802af4a0
Samuel RM2910 <albeirito2910@gmail.com>
2024-12-12 09:39:26 -0500

    act13

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Footer from './components/Footer'
 
 import Home from './pages/Home';
