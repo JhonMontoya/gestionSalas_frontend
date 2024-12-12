7ee6a35c2107af057ecee65c80357a80aa91e523
Jhonathan <jhonathan.guerrero.96@gmail.com>
2024-12-12 12:31:26 -0500

    cambios iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import NavBar from './components/Navbar';
 import Footer from './components/Footer'
 
 import Home from './pages/Home';
-import Register from './pages/Register';
 import { Halls } from "./pages/Halls";
-
 
 
 function App(){
   return (
-    <Router>
-      <NavBar>
-      </NavBar>
-      
+    <Router>      
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/register" element={<Register/>}/>
