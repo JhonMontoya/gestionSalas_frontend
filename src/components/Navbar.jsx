827bde9cf3a99c477bfedb311b0c1ecb61e93123
Leslymatabajoy <letsly.matabajoy@correounivalle.edu.co>
2024-12-12 01:13:06 -0500

    logro: añadir menu a la navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,116 @@
-import * as React from 'react';
-import {Link} from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
+//eslint-disable-next-line
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-
 import logo from "../assets/Logo.png";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Avatar,
+  Tooltip,
+  IconButton,
+  MenuItem,
+  Menu,
+  Typography,
+  Container,
+} from "@mui/material";
+
+const settings = [
+  { key: 1, label: "Perfil" },
+  { key: 2, label: "Cuenta" },
+  { key: 3, label: "Iniciar sesion" },
+  { key: 4, label: "Cerrar sesion" },
+];
 
 export default function ButtonAppBar() {
+  //aqui se debe setear el usuario que este logeado.
+  //manejarlo con un estado como redux o sustand o con un localStorage.
+  const user = {
+    id: "674d47dcc5d0b949e5fe20f1",
+    name: "Duvan Camilo Zapata",
+    email: "prueba@email.com",
+    thumnail:
+      "https://media.glamour.mx/photos/65b096f13756393e0200c63d/16:9/w_1920,c_limit/que-significa-tu-foto-de-perfil.jpg",
+    role: "single",
+    //password: "$2a$10$PDH8l8ZLpdiZJtbKK3cA/.iXQcvBhSF7mb4deMswd/aeHGIp0lpWC",
+    v: 0,
+  };
+
+  const [userMenu, setUserMenu] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setUserMenu(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setUserMenu(null);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{backgroundColor: "#8787f5"}}>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', width: '95%' }}>
-          <Link to = "/" style={{textDecoration:'none'}}>
-          <img
-            src={logo}
-            style={{width: '70px'}}
-            />
-          </Link>
-          <Button
-            variant="outlined"
-            color="inherit"
-            startIcon={<AccountCircleIcon />}
-            >Acceder</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static" sx={{ backgroundColor: "#8787f5" }}>
+      <Container maxWidth="xl">
+        <Box sx={{ flexGrow: 1 }}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "95%",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center", gap: "40px" }}>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <img src={logo} style={{ width: "70px" }} />
+              </Link>
+              <IconButton
+                style={{ borderRadius: "10px" }}
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <Typography sx={{ textAlign: "center" }}>
+                  Crear reservas
+                </Typography>
+              </IconButton>
+            </Box>
+            <Tooltip title="Abrir configuración">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar src={user ? user.thumnail : <AccountCircleIcon />} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={userMenu}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(userMenu)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem
+                  disabled={user ? setting.key === 3 : setting.key === 4}
+                  key={setting.key}
+                  onClick={handleCloseUserMenu}
+                >
+                  <Typography sx={{ textAlign: "center" }}>
+                    {setting.label}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Toolbar>
+        </Box>
+      </Container>
+    </AppBar>
   );
 }
