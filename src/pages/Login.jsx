54769e5d19ff2b752277b27789e90ccc5b02304b
Samuel RM2910 <albeirito2910@gmail.com>
2024-12-12 19:20:56 -0500

    act14

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/login.css";
 import logo from "../assets/Logo.png";
